8752d4f3bf511787380fe0ddfdd12fdfbb4312f3
Eugene Tereshchenko <i@jman.info>
2023-09-16 12:12:34 +0200

    chore: formatting

--- e2e/vue.spec.cjs
+++ e2e/vue.spec.cjs
@@ -1,7 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-
 test('screenshot', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveScreenshot({ fullPage : true });
-})
+	await page.goto('/');
+	await expect(page).toHaveScreenshot({ fullPage: true });
+});
