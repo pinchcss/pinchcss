7427632209cde1f488ea963609c8974ff7df27ff
Eugene Tereshchenko <i@jman.info>
2023-09-21 15:14:36 +0200

    test: add screenshot testing per page

--- e2e/vue.spec.cjs
+++ e2e/vue.spec.cjs
@@ -1,6 +1,16 @@
 const { test, expect } = require('@playwright/test');
 
-test('screenshot', async ({ page }) => {
+test('typography', async ({ page }) => {
 	await page.goto('/');
+	await expect(page).toHaveScreenshot({ fullPage: true });
+});
+
+test('inputs', async ({ page }) => {
+	await page.goto('/inputs');
+	await expect(page).toHaveScreenshot({ fullPage: true });
+});
+
+test('buttons', async ({ page }) => {
+	await page.goto('/buttons');
 	await expect(page).toHaveScreenshot({ fullPage: true });
 });
