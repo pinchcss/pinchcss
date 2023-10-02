307abe7495ffeff62f250b14310994aadbde8aa3
Eugene Tereshchenko <i@jman.info>
2023-10-02 17:11:51 +0200

    chore: fix buttons a11y, formatting

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import ExamplesTypography from '../views/ExamplesTypography.vue'
+import ExamplesTypography from '../views/ExamplesTypography.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: ExamplesTypography,
-    },
-    {
-      path: '/inputs',
-      name: 'inputs',
-      component: () => import('../views/ExamplesInputs.vue'),
-    },
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
 		{
-      path: '/buttons',
-      name: 'buttons',
-      component: () => import('../views/ExamplesButtons.vue'),
-    }
-  ]
-})
+			path: '/',
+			name: 'home',
+			component: ExamplesTypography,
+		},
+		{
+			path: '/inputs',
+			name: 'inputs',
+			component: () => import('../views/ExamplesInputs.vue'),
+		},
+		{
+			path: '/buttons',
+			name: 'buttons',
+			component: () => import('../views/ExamplesButtons.vue'),
+		},
+	],
+});
 
 export default router;
